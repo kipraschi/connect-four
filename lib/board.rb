24b80b5cab70802cc1324ee37059d31d0c435e1e
Kipras Chi <kipraschi@gmail.com>
2026-05-29 21:25:38 +0300

    feat: add Board#valid_move? method

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,24 @@
 class Board
   attr_reader :grid
-  def initialize
-    @height = 6
-    @width = 7
-    @empty_cell = '〇'
+  def initialize (height = 6, width = 7)
+    @height = height
+    @width = width
+    @empty_cell = '◯'
     @grid = Array.new(@width) { Array.new(@height) {@empty_cell} }
   end
 
   def display
-    puts @grid.transpose.map { |row| row.join }
+    puts @grid.transpose.map { |row| row.join(' ') }
   end
 
   def update(column_index, symbol)
     column = @grid[column_index]
     index_of_last_empty_cell = column.rindex(@empty_cell)
     @grid[column_index][index_of_last_empty_cell] = symbol
+  end
+
+  def valid_move?(column_index)
+    return false unless column_index.between?(0, @width - 1)
+    @grid[column_index].any?(@empty_cell)
   end
 end
