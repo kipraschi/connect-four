0e6583c5de8f2cbb158542d386a10f2e651f0954
Kipras Chi <kipraschi@gmail.com>
2026-06-03 22:54:58 +0300

    feat: add Player#choose_column method with #valid_input? helper

---
+++ lib/player.rb
@@ -0,0 +1,18 @@
+class Player
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def choose_column
+    loop do
+      input = gets.chomp
+      return input.to_i if valid_input?(input)
+    end
+  end
+
+  private
+  
+  def valid_input?(input)
+    input.match?(/\A[1-7]\z/)
+  end
+end
