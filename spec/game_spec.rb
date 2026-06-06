186ea81100ac80dd982d401bc8d9fda26d093c5c
Kipras Chi <kipraschi@gmail.com>
2026-06-06 21:07:40 +0300

    test: suppress puts output in Game#play tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Game do
     context 'until the game is over' do
       before do
         allow(game).to receive(:announce).with(any_args)
+        allow(game).to receive(:puts)
         allow(game).to receive(:game_over?).and_return(false, false, false, true)
         allow(player1).to receive(:choose_column).and_return(4)
         allow(player2).to receive(:choose_column).and_return(3)
@@ -36,6 +37,7 @@ RSpec.describe Game do
     context 'when one of the selected columns is not available' do
       before do
         allow(game).to receive(:announce).with(any_args)
+        allow(game).to receive(:puts)
         allow(game).to receive(:game_over?).and_return(false, true)
         allow(player1).to receive(:choose_column).and_return(8, 3)
         allow(board).to receive(:update)
